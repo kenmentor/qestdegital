d8fbe694f233fafe3eb7f5cba8de3b2e79429a58
mentor <davidnwachukwum@gmail.com>
2026-05-06 20:34:21 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,6 +57,7 @@ export default function Home() {
       router.push(`/meeting/${selectedLang}`);
     }
   };
+  console.log("hello world for debuging ")
 
   return (
     <div className="min-h-screen bg-[#0a0a0a] flex flex-col items-center justify-center p-4 sm:p-6">
